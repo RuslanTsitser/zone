7080324a264558721f13f28bf003e327d87c53ff
RuslanTsitser <cicer_ruslan@mail.ru>
2024-06-05 14:59:01 +0300

    refactor: Refactor main.dart for error handling and isolate communication
    This commit refactors the main.dart file to improve error handling and isolate communication. The changes include:
    - Adding error handling using runZonedGuarded to catch and handle errors in a standardized way.
    - Implementing FlutterError.onError and WidgetsBinding.instance.platformDispatcher.onError to catch and handle errors in specific contexts.
    - Adding isolate communication using SendPort and ReceivePort to enable communication between the main isolate and a remote isolate.
    - Adding a spawn() method to spawn a remote isolate and handle responses from it.
    
    These changes aim to enhance the stability and reliability of the application by providing better error handling and enabling isolate communication.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,82 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  // try {
+  //   fineMethod();
+  // } catch (e) {
+  //   debugPrint('RootZone Caught error standard way: $e');
+  // }
+
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      // try {
+      //   // await badMethod();
+      //   await fineMethod();
+      // } catch (e) {
+      //   debugPrint('Caught error standard way: $e');
+      // }
+      FlutterError.onError = (details) {
+        debugPrint('Caught error in FlutterError.onError: ${details.exception}');
+      };
+      WidgetsBinding.instance.platformDispatcher.onError = (err, st) {
+        debugPrint('Caught error in platformDispatcher.onError: $err');
+        return true;
+      };
+      runApp(const MyApp());
+    },
+    (e, s) {
+      debugPrint('Caught error in runZonedGuarded.onError: $e');
+    },
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late SendPort _sendPort;
+  final Completer<void> _isolateReady = Completer.sync();
+  static void _startRemoteIsolate(SendPort port) {
+    final receivePort = ReceivePort();
+    port.send(receivePort.sendPort);
+    runZonedGuarded(
+      () async {
+        receivePort.listen((dynamic message) async {
+          throw 'Isolate exception message: $message';
+          // port.send(message);
+        });
+      },
+      (e, s) {
+        port.send(RemoteError(
+          e.toString(),
+          s.toString(),
+        ));
+      },
+    );
+  }
+
+  Future<void> spawn() async {
+    final receivePort = ReceivePort();
+    receivePort.listen(_handleResponsesFromIsolate);
+    await Isolate.spawn(_startRemoteIsolate, receivePort.sendPort);
+  }
+
+  void _handleResponsesFromIsolate(dynamic message) {
+    if (message is SendPort) {
+      _sendPort = message;
+      _isolateReady.complete();
+    } else if (message is RemoteError) {
+      throw message;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +86,55 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FloatingActionButton(
+              child: const Text('spawn'),
+              onPressed: () {
+                // asyncError(); // could be caught by runZonedGuarded.onError
+                // Future(() => throw "Future exception"); // could be caught by runZonedGuarded.onError
+                // syncError(); // could be caught by FlutterError.onError
+                // throw "Flutter exception"; // could be caught by FlutterError.onError
+                // compute((_) => asyncError(), 'message');
+                // compute((_) => syncError(), 'message');
+                spawn();
+              },
+            ),
+            const SizedBox(height: 8),
+            FloatingActionButton(
+              onPressed: () {
+                // asyncError(); // could be caught by runZonedGuarded.onError
+                // Future(() => throw "Future exception"); // could be caught by runZonedGuarded.onError
+                // syncError(); // could be caught by FlutterError.onError
+                // throw "Flutter exception"; // could be caught by FlutterError.onError
+                // compute((_) => asyncError(), 'message');
+                // compute((_) => syncError(), 'message');
+                // spawn();
+                _sendPort.send('message');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+void syncError() {
+  throw "Sync error";
+}
+
+Future<void> asyncError() async {
+  Future.delayed(const Duration(seconds: 1), () => throw "Future exception");
+  await Future(() {});
+}
+
+Future<void> fineMethod() async {
+  await asyncError();
+}
+
+Future<void> badMethod() async {
+  asyncError();
 }
